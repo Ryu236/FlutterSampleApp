08de56ae9e1d148c87b2ec1f347aa1f92fedc60d
Ryu236 <ryu.adv.2360@gmail.com>
2018-03-28 20:04:31 +0900

    add english_words and print a random word.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new SampleApp());
 
 class SampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Flutter Smaple App',
+      debugShowCheckedModeBanner: false,
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Flutter Sample App'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          //child: new Text('Hello World!'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
